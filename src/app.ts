b21da2d459e2ccc8058d7ba5704281552db45927
MateusR <mateusftc_@hotmail.com>
2023-10-26 21:33:40 -0300

    feat: Criado o Router de teste

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import config from "config";
+import router from "./router";
 
 const app = express();
 
 app.use(express.json());
 
 const port = config.get<number>("port");
+
+app.use("/api/", router);
 
 app.listen(port, async () => {
   console.log(`listening on port ${port}`);
