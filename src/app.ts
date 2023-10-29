387e372373fadb19d6e5742943af05cad3dd7e43
MateusR <mateusftc_@hotmail.com>
2023-10-29 02:24:20 -0300

    feat: Adicionado o MorganMiddleware ao projeto

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,15 @@ import config from "config";
 import router from "./router";
 import db from "../config/db";
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/MorganMiddleware";
 
 const app = express();
 
 app.use(express.json());
 
 const port = config.get<number>("port");
+
+app.use(morganMiddleware);
 
 app.use("/api/", router);
 
