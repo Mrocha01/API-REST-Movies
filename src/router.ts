612827e264f29cf20689b7c9f4bf6fa8d11ffcd8
MateusR <mateusftc_@hotmail.com>
2023-10-30 00:35:15 -0300

    feat: Primeiras configurações do Controller

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieController";
 
 const router = Router();
 
-export default router.get("/test", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Welcome!" });
-});
+export default router
+  .get("/test", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Welcome!" });
+  })
+  .post("/movie", createMovie);
